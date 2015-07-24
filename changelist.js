b08e27fe515a6e8c4bc74588c109ab802536bbd1
Dan Petty <mixmatch@gmail.com>
2015-07-24 14:30:17 -0600

    Support for IE8, main loop for accordion, material icons

--- changelist.js
+++ changelist.js
@@ -3,58 +3,170 @@
 //   type: "", // string - Firewall, Build
 //   build: "", // string - Build ID
 //   owner: "", // string - User
-//   startTimestamp: , // number - Unix Epoch Date test started
+//   startTimestamp: 0, // number - Unix Epoch Date test started
 //   state: "", // string - Pending, Running, Complete, Rejected, Accepted
 //   metrics: {
-//     test: , // number - test metricts
-//     maintainability: , // number - maintainability metricts
-//     security: , // number - security metricts
-//     worksmanship:  // number - worksmanship metricts
+//     status: "", // string - Running, Passed, Failed
+//     test: 0, // number - test metricts
+//     maintainability: 0, // number - maintainability metricts
+//     security: 0, // number - security metricts
+//     worksmanship: 0 // number - worksmanship metricts
 //   },
 //   build: {
-//     debug: , // bool debug build passed
-//     release: , // bool release build passed
-//     buildTimestamp: , // number - Unix Epoch Date build completed
+//     status: "", // string - Running, Passed, Failed
+//     debug: 0, // bool debug build passed
+//     release: 0, // bool release build passed
+//     buildTimestamp: 0, // number - Unix Epoch Date build completed
 //   },
 //   unitTest: {
-//     numberPassed: , // number - unit tests passed
-//     numberFailed: , // number - unit tests failed
-//     codeCovered: // number - percent of code covered 0-100
+//     status: "", // string - Running, Passed, Failed
+//     numberPassed: 0, // number - unit tests passed
+//     numberFailed: 0, // number - unit tests failed
+//     codeCovered: 0 // number - percent of code covered 0-100
 //   },
 //   functionalTest: {
-//     numberPassed: , // number - functional tests passed
-//     numberFailed: , // number - functional tests failed
-//     codeCovered: // number - percent of code covered 0-100
+//     status: "", // string - Running, Passed, Failed
+//     numberPassed: 0, // number - functional tests passed
+//     numberFailed: 0, // number - functional tests failed
+//     codeCovered: 0 // number - percent of code covered 0-100
 //   }
 // }
 
-data = [
-  {
-    type: "", // Firewall, Build
-    build: "",
-    owner: "",
-    startTimestamp: "",
-    state: "", //Pending, Running, Complete, Rejected, Accepted
-    metrics: {
-      test: ,
-      maintainability: ,
-      security: ,
-      worksmanship: 
-    },
-    build: {
-      debug: ,
-      release: ,
-      buildTimestamp: "",
-    },
-    unitTest: {
-      numberPassed: ,
-      numberFailed: ,
-      codeCovered:
-    },
-    functionalTest: {
-      numberPassed: ,
-      numberFailed: ,
-      codeCovered:
-    }
-  }
-];
+//
+angular.module('app', ['ui.bootstrap']).controller('ChangelistAccordion', function ($scope) {
+  
+  var getData = function() {
+    var data = [
+      {
+        type: "Build", // Firewall, Build
+        build: "Tenrox-R1_1235",
+        owner: "",
+        startTimestamp: 0,
+        state: "Pending", //Pending, Running, Complete, Rejected, Accepted
+        metrics: null,
+        build: null,
+        unitTest: null,
+        functionalTest: null
+      },
+      {
+        type: "Firewall", // Firewall, Build
+        build: "432462",
+        owner: "jtuck",
+        startTimestamp: 1397844720,
+        state: "Running", //Pending, Running, Complete, Rejected, Accepted
+        metrics: {
+          test: 64,
+          maintainability: 53,
+          security: 64,
+          worksmanship:0
+        },
+        build: null,
+        unitTest: null,
+        functionalTest: null
+      },
+      {
+        type: "Firewall", // Firewall, Build
+        build: "432461",
+        owner: "samy",
+        startTimestamp: 1397839980,
+        state: "Rejected", //Pending, Running, Complete, Rejected, Accepted
+        metrics: {
+          test: 64,
+          maintainability: 53,
+          security: 64,
+          worksmanship:72
+        },
+        build: {
+          debug: true,
+          release: true,
+          buildTimestamp: 1397854200,
+        },
+        unitTest: {
+          numberPassed: 142,
+          numberFailed: 10,
+          codeCovered: 76
+        },
+        functionalTest: {
+          numberPassed: 4321,
+          numberFailed: 2145,
+          codeCovered: 23
+        }
+      },
+      {
+        type: "Build", // Firewall, Build
+        build: "Tenrox-R1_1234",
+        owner: "",
+        startTimestamp: 1397749320,
+        state: "Complete", //Pending, Running, Complete, Rejected, Accepted
+        metrics: {
+          test:64,
+          maintainability: 53,
+          security: 64,
+          worksmanship:72
+        },
+        build: {
+          debug: true,
+          release: true,
+          buildTimestamp: 1397764200,
+        },
+        unitTest: {
+          numberPassed: 142,
+          numberFailed: 10,
+          codeCovered: 76
+        },
+        functionalTest: {
+          numberPassed: 4321,
+          numberFailed: 2145,
+          codeCovered: 23
+        }
+      },
+      {
+        type: "Firewall", // Firewall, Build
+        build: "432460",
+        owner: "samy",
+        startTimestamp: 1397742660,
+        state: "Rejected", //Pending, Running, Complete, Rejected, Accepted
+        metrics: {
+          test: 0,
+          maintainability: 0,
+          security: 0,
+          worksmanship:0
+        },
+        build: null,
+        unitTest: null,
+        functionalTest: null
+      },
+      {
+        type: "Firewall", // Firewall, Build
+        build: "432459",
+        owner: "samy",
+        startTimestamp: 1397652180,
+        state: "Accepted", //Pending, Running, Complete, Rejected, Accepted
+        metrics: {
+          test: 64,
+          maintainability: 53,
+          security: 64,
+          worksmanship:72
+        },
+        build: {
+          debug: true,
+          release: true,
+          buildTimestamp: 1397753160,
+        },
+        unitTest: {
+          numberPassed: 142,
+          numberFailed: 10,
+          codeCovered: 76
+        },
+        functionalTest: {
+          numberPassed: 4321,
+          numberFailed: 2145,
+          codeCovered: 23
+        }
+      },
+    ];
+    return data
+  };
+
+  $scope.changeList = getData();
+});
